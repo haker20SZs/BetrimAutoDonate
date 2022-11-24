6c32c715aff3ff2927e25e99438afb9f8774c1e1
Zloger <79506370+haker20SZs@users.noreply.github.com>
2022-11-25 05:30:33 +0700

    Add files via upload

---
+++ Trading Platforms/EasyDonate/api.php
@@ -0,0 +1,42 @@
+<?php
+
+require './config.php';
+
+if($_GET["key"] === $secret_word){
+
+$products = curl_init('https://easydonate.ru/api/v3/shop/products');
+curl_setopt($products, CURLOPT_HTTPHEADER, array('Shop-Key:' . $key));
+curl_setopt($products, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($products, CURLOPT_SSL_VERIFYPEER, true);
+
+$pay = curl_exec($products);
+curl_close($products);
+
+$sys_products = json_decode($pay, true)["response"][$_GET["id"]]['id'];
+
+$get = array(
+	'customer'  => $_GET["nick"],
+	'server_id' => intval($serverid),
+	'email' => $email,
+	'coupon' => $_GET["ref"],
+	'products' => '{"' . $sys_products . '":1}',
+	'success_url' => $_GET["success_url"]
+);
+
+$get = curl_init('https://easydonate.ru/api/v3/shop/payment/create?' . http_build_query($get));
+curl_setopt($get, CURLOPT_HTTPHEADER, array('Shop-Key:' . $key));
+curl_setopt($get, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($get, CURLOPT_SSL_VERIFYPEER, true);
+
+$pay = curl_exec($get);
+curl_close($get);
+
+$sys_pay = json_decode($pay, true)["response"]['url'];
+
+header("Location: " . $sys_pay);
+
+}else{
+	header("Location: /");
+}
+
+?>
---
+++ Trading Platforms/EasyDonate/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = "MilordPE"; // Названия сайта
+$serverid = ""; // ID сервера
+$key = ""; // Ключ магазина
+$email = null; // Почта для чеков - null
+$secret_word = "key"; // Секретный ключ для проверки
+
+//api.php?nick=steve&id=0&success_url=https://bots.ru&key=key
+
+?>
