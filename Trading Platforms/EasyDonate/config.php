52d986401df0c398244b7e92a144fe83aa0fa192
Zloger <79506370+haker20SZs@users.noreply.github.com>
2022-11-25 05:37:18 +0700

    Update config.php

--- Trading Platforms/EasyDonate/config.php
+++ Trading Platforms/EasyDonate/config.php
@@ -4,7 +4,7 @@ $name = "MilordPE"; // Названия сайта
 $serverid = ""; // ID сервера
 $key = ""; // Ключ магазина
 $email = null; // Почта для чеков - null
-$urlweb = "https://milord-pe.ru/";
+$urlweb = "https://milord-pe.ru/"; // Ссылка на веб сайт
 $secret_word = "key"; // Секретный ключ для проверки
 
 //api.php?nick=steve&id=0&success_url=https://bots.ru&key=key
