6b1ab1040c5213ad4c2eeec794b2cce296fb8d03
Zloger <79506370+haker20SZs@users.noreply.github.com>
2022-11-25 05:58:50 +0700

    Update config.php

--- Trading Platforms/EasyDonate/config.php
+++ Trading Platforms/EasyDonate/config.php
@@ -4,7 +4,7 @@ $name = "BetrimAutoDonate"; // Названия сайта
 $serverid = ""; // ID сервера
 $key = ""; // Ключ магазина
 $email = null; // Почта для чеков - null
-$urlweb = "https://autodonate/"; // Ссылка на веб сайт
+$urlweb = "http://autodonate/"; // Ссылка на веб сайт
 $secret_word = "key"; // Секретный ключ для проверки
 
 //api.php?nick=steve&id=0&success_url=https://bots.ru&key=key
